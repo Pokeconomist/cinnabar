c0b1dee604822922aba765390c979333790491da
Pokeconomist <Pokeconomist@users.noreply.github.com>
2017-07-16 19:31:09 +1000

    Reflactor of Deck creation to seperate file
    Deck class / set class change to Deck module / set class in file `./modules/deck.rb`

--- cinnabar.rb
+++ cinnabar.rb
@@ -18,119 +18,9 @@
 # i.e.
 #   array of sets (with attributes set_num and set_data, comprised of set_name, and set_cards , which in turn is comprised of set_pos, and card_data, which is comprised of card_name, and card_desc)
 require 'csv'
+require './modules/deck'
 
-# class to create set elements from csv
-class Set
-  attr_reader :set
-
-  # get arrays from csv files, and convert relevent keys to int
-  @@cards = CSV.read('files/cards.csv').each do |cards|
-    cards.map!.each_with_index do |element, index|
-      index == 1 ? element.to_i : element
-    end
-  end
-  @@sets = CSV.read('files/sets.csv').each do |cards|
-    cards.collect!.each_with_index do |element, index|
-      index == 0 || index == 2 ? element.to_i : element
-    end
-  end
-
-  # create array of all cards within a set
-  def self.compile_cards(set_num)
-    # find cards of set number == set_num, and return their data as a hash
-    @@cards.select { |card| card[1] == set_num }.collect { |card| {:set_pos => card[2], :card_data => {:card_name => card[0], :card_desc => card[3]} } }
-  end
-
-  # create hash for a set
-  def initialize(line_num)
-    set_num = @@sets[line_num][0]
-    set_name = @@sets[line_num][1]
-    set_length = @@sets[line_num][2]
-    @set = {:set_num => set_num, :set_data => {:set_name => set_name, :set_length => set_length, :set_cards => (Set.compile_cards(set_num))}}
-  end
-end
-# class to create the deck from all sets (12)
-class Deck
-  attr_reader :deck
-
-  # create hash of set elements,
-  def initialize
-    @@deck = Array.new(12) { |n| Set.new(n).set }
-  end
-
-  # create hash of card ids with mname as key (eg. {:cinnabar => [1, "A"]...})
-  def self.get_id_array
-    cards = []
-    @@deck.each do |set|
-      set[:set_data][:set_cards].each do |card|
-        cards << [set[:set_num], card[:set_pos]]
-      end
-    end
-    return cards
-  end
-
-  # method to return data for a specified card
-  def self.get_card_data(set_num, set_pos)
-    @@deck.each do |set|
-      # find set
-      if set[:set_num] == set_num
-        # terate over set
-        set[:set_data][:set_cards].each do |card|
-          # find card
-          if card[:set_pos] == set_pos
-            return card[:card_data][:card_name], card[:card_data][:card_desc], set[:set_data][:set_name], set[:set_num], card[:set_pos]
-          end
-        end
-      end
-    end
-  end
-
-  # function to return other cards in set
-  def self.get_card_set(set_num, set_pos)
-    card_set = []
-    @@deck.each do |set|
-      # find set
-      if set[:set_num] == set_num
-        # iterate over set
-        set[:set_data][:set_cards].each do |card|
-          # find all other cards
-          if card[:set_pos] != set_pos
-            card_set << card[:card_data][:card_name]
-          end
-        end
-        return card_set
-      end
-    end
-  end
-
-  # method to get a cards id from its name (ad validates input)
-  def self.get_card_id(card_name)
-    card_name = card_name.to_s.downcase.capitalize
-    @@deck.each do |set|
-      set[:set_data][:set_cards].each do |card|
-        if card[:card_data][:card_name] == card_name
-          return set[:set_num], card[:set_pos]
-        end
-      end
-    end
-  end
-
-  # method to check a had for complete sets
-  def self.check_set(hand)
-    # get all unique sets in hand
-    sets = hand.collect { |card| card[0] }.uniq
-    # check if each set complete
-    sets.each do |set_num|
-      print set_num
-      print @@deck.select { |set| set[:set_num] == set_num  }
-     complete_sets <<
-      if hand.count { |card| card[0] == test_card[0] } == @@deck[:set_data][:set_length]
-        {:set_num => test_card[0]}
-      end
-    end
-    return complete_sets
-  end
-end
+# WIP: moving old deck class to module (and possible taking set with it)
 
 # class to handle players (mainly turn functions)
 class Player
@@ -139,11 +29,11 @@ class Player
   # init method, that creates all player attr, and a diminished reserve variable (only used by Game class)
   def initialize(player_num, cards)
     @player_num = player_num
-    @hand, @reserve = self.get_hand(cards)
+    @hand, @reserve = self.create_hand(cards)
   end
 
   # method to get a players hand (sorted), and to return a decreased reserve pile
-  def get_hand(cards)
+  def create_hand(cards)
     hand = cards.sample(6)
     cards -= hand
     return hand.sort, cards
@@ -152,24 +42,24 @@ class Player
   # method to get a players inputs
   def player_turn
     puts 'Your cards are'
-    @hand.each { |card|  print_card(*Deck.get_card_data(*card), Deck.get_card_set(*card)) }
+    @hand.each { |card| print_card(*Deck.card_data(*card), Deck.card_set(*card)) }
 
-    # TODO: find solution that doesn't use instance variables
+    # TODO: find solution that doesn't use instance variables, but allows access from other scopes
 
     # get and check input for card name
-    @wanted_card_id = get_wanted_card()
+    @wanted_card_id = input_wanted_card()
     # get and check input for player number
-    @called_player = get_wanted_player()
+    @called_player = input_wanted_player()
   end
 
   # method to get / check a players wanted card input
-  def get_wanted_card
+  def input_wanted_card
     loop do
       print 'What card do you want (only from sets you have): '
       wanted_card_name = gets.chomp
       # check against sets, and validate input
-      if (@hand.collect { |e| e[0] }.include? (Deck.get_card_id(wanted_card_name)[0])) && !(@hand.include? (Deck.get_card_id(wanted_card_name)))
-        return Deck.get_card_id(wanted_card_name)
+      if (@hand.collect { |e| e[0] }.include? (Deck.card_id(wanted_card_name)[0])) && !(@hand.include? (Deck.card_id(wanted_card_name)))
+        return Deck.card_id(wanted_card_name)
       else
         puts 'Please enter a valid card name...'
       end
@@ -177,7 +67,7 @@ class Player
   end
 
   # method to get / check wanted player input
-  def get_wanted_player
+  def input_wanted_player
     loop do
       print 'What player do you call: '
       called_player = gets.chomp.to_i
@@ -202,15 +92,18 @@ class Player
   end
 end
 
-# @turn_data instance variable used to specify what occured during the last turn, and notify other players
+# Handling of Turn Data
+# --
+# @turn_data instance variable used to specify what occurred during the last turn, and notify other players
 # @turn_data = [{:card_taken => $bool$, :called_player => $int$, card => $arr$}, ...]
 # card_taken true of player [:called_player] has [:card]
-# can check for all cases with these vars
+# can check for all cases with these variables
 # ie.
-#   p call p[n] - doesn't have card / has card
-#             |        |-> call again >> @turn_data
-#           end turn
-# thus @turn_array[-1] == false for all cases
+# :A p call p[n] - doesn't have card / has card
+#                             |          |-> call again >> @turn_data && goto A
+#                            \/
+#                         end turn
+# __thus @turn_array[-1] == false for all end cases__
 
 # class to handle game turns
 class Game
@@ -228,7 +121,7 @@ class Game
     @players = [@player1, @player2, @player3]
   end
 
-  # method to define standard turn code (handles ineraction of player objects)
+  # method to define standard turn code (handles interaction of player objects)
   def game_turn
     (0..2).each do |n|
       cls
@@ -236,9 +129,9 @@ class Game
       if @turn_data != []
         @turn_data.each do |card|
           if card[:card_taken]
-            puts "Player #{@players[n - 1].player_num} took #{Deck.get_card_data(*card[:card])[0]} from Player #{card[:called_player]}."
+            puts "Player #{@players[n - 1].player_num} took #{Deck.card_data(*card[:card])[0]} from Player #{card[:called_player]}."
           else
-            puts "Player #{@players[n - 1].player_num} asked Player #{card[:called_player]} for #{Deck.get_card_data(*card[:card])[0]} but was denied."
+            puts "Player #{@players[n - 1].player_num} asked Player #{card[:called_player]} for #{Deck.card_data(*card[:card])[0]} but was denied."
           end
         end
       end
@@ -257,13 +150,14 @@ class Game
           break
         end
       end
-      pp Deck.check_set(@players[n].hand)
+      p Deck.check_set(@players[n].hand)
+      pause
       # TODO: Add check for set, no cards, and win
 
     end
   end
 
-  # TODO: loop if card_taken == true //DONE
+  # TODO: loop if card_taken == true // DONE
 
   # method to check if a card is present in a players hand (and to update turn_data)
   def check_card(player_num, called_player_num, card_id)
@@ -274,7 +168,7 @@ class Game
         print "Player #{called_player_num} had the card"
         true
       else
-        print "Player #{called_player_num} didn't have the card..."
+        puts "Player #{called_player_num} didn't have the card..."
         false
       end
     @turn_data << {:card_taken => card_taken, :called_player => called_player_num, :card => card_id}
@@ -286,25 +180,28 @@ class Game
     added_card = @@reserve.sample
     @players[player_index].add_card(added_card)
     @@reserve -= [added_card]
-    print "#{Deck.get_card_data(*added_card)[0]} was drawn."
+    print "#{Deck.card_data(*added_card)[0]} was drawn."
     pause
   end
 end
 
 # independent functions
 def pause
-  system('pause')
+  system('pause>nul')
 end
 
 def cls
   system('cls')
 end
 
+# TODO: fix display of cards (eliminate hash - error possibly stemming from Deck::card_set)
+
 # game functions
 # function that prints cards data
 def print_card(card_name, card_desc, set_name, set_num, set_pos, card_set)
   puts '----------------'
-  card_set.each { |card| puts " #{card}" }
+  puts card_set
+  #card_set.each { |card| puts " #{card}" }
   puts "#{set_name.upcase}    #{set_num}-#{set_pos}"
   puts "  #{card_name.upcase}"
   puts "(#{card_desc})"
@@ -312,15 +209,13 @@ def print_card(card_name, card_desc, set_name, set_num, set_pos, card_set)
 end
 
 # single line card print for debugging
-def quick_print_card(card_name, card_desc, set_name, set_num, set_pos, card_set)
+def quick_print_card(card_name, _card_desc, _set_name, _set_num, _set_pos, _card_set)
   puts card_name
 end
 
-# init deck storage
-Deck.new
 
 # array of acceptable card ids (static list), eg. [[1, "A"], [1, "B"],...]
-cards = Deck.get_id_array
+cards = Deck.compile_id_array
 
 # main loop
 turn = 0
@@ -329,7 +224,7 @@ loop do
     # init game object
     game = Game.new(cards)
     game.game_turn
-    # TODO: implement better reserve pile handling //wontfix
+    # TODO: implement better reserve pile handling // wontfix
 
     turn += 1
   end
