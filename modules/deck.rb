4f5db9f675e420828428ac914272181dd22340b8
Pokeconomist <26198040+Pokeconomist@users.noreply.github.com>
2017-12-19 00:40:15 +1100

    Added card_set method

--- modules/deck.rb
+++ modules/deck.rb
@@ -57,31 +57,47 @@ module Deck
     }
   end
 
-  # method to retrieve card data from id
+  # method to return array of card data from id
   def card_data(set_num, set_pos)
-    # find set
     DECK.each do |set|
+      # skip iteration unless desired set number
       next unless set[:set_num] == set_num
-      # find card
       set[:set_data][:set_cards].each do |card|
+        # skip iteration unless desired set position
         next unless card[:set_pos] == set_pos
         return card[:card_data][:card_name], card[:card_data][:card_desc], set[:set_data][:set_name], set[:set_num], card[:set_pos]
       end
     end
   end
 
-  # method to get card id from name
+  # method to return card id from name
   def card_id(card_name)
     card_name = card_name.to_s.downcase.capitalize
     # iterate over sets and cards, and check for specific card name
     DECK.each do |set|
       set[:set_data][:set_cards].each do |card|
-        if card[:card_data][:card_name] == card_name
-          return set[:set_num], card[:set_pos]
-        end
+        # skip iteration unless desired card name
+        next unless card[:card_data][:card_name] == card_name
+        return set[:set_num], card[:set_pos]
       end
     end
   end
+
+  # method to return array of other cards in a set from one card's id
+  def card_set(set_num, set_pos)
+    card_set = []
+    DECK.each do |set|
+      # skip iteration unless desired set number
+      next unless set[:set_num] == set_num
+      set[:set_data][:set_cards].each do |card|
+        # skip iteration if initial card
+        next if card[:set_pos] == set_pos
+        card_set << card[:card_data][:card_name]
+      end
+      return card_set
+    end
+  end
+
 end
 
 # TODO: Implement deck data access methods (~card_data~, ~card_id~, card_set)
