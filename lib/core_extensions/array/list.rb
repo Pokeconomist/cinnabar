20ab45c31b78569fd13a9e60f8f194d8c1e8eb8f
Pokeconomist <26198040+Pokeconomist@users.noreply.github.com>
2018-01-15 22:53:18 +1100

    Clarify sentence delims

--- lib/core_extensions/array/list.rb
+++ lib/core_extensions/array/list.rb
@@ -1,15 +1,20 @@
 # monkey patch to allow conversion of array of strings to sentence
 class Array
   def to_list
+    default_connectors = {
+      words_connector:        ', ',
+      two_words_connector: ' and ',
+      last_word_connector: ', and '
+    }
     case self.length
     when 0
       ''
     when 1
       self[0].to_s.dup
     when 2
-      "#{self[0]} and #{self[1]}"
+      "#{self[0]}#{options[:two_words_connector]}#{self[1]}"
     else
-      "#{self[0...-1].join(", ")}, and #{self[-1]}"
+      "#{self[0...-1].join(options[:words_connector])}#{options[:last_word_connector]}#{self[-1]}"
     end
   end
 end
