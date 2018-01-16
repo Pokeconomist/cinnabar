8129781f068c8479e15d5b6f0483dbbb0faf0d81
Pokeconomist <26198040+Pokeconomist@users.noreply.github.com>
2018-01-16 22:45:18 +1100

    remove `return` statements

--- src/modules/deck.rb
+++ src/modules/deck.rb
@@ -20,7 +20,7 @@ module Deck
   # method to return array of card data from id
   def card_data(set_num, set_pos)
     card = CARDS.select { |card| card[:setNumber] == set_num && card[:setPosition] == set_pos }[0]
-    return card.nil? ? nil : [
+    card.nil? ? nil : [
       card[:cardName],
       card[:cardDescription],
       set_data(card[:setNumber])[1],
@@ -33,7 +33,7 @@ module Deck
   def card_id(card_name)
     card_name = card_name.to_s.downcase.capitalize
     card = CARDS.select { |card| card[:cardName] == card_name }[0]
-    return card.nil? ? nil : [
+    card.nil? ? nil : [
       card[:setNumber],
       card[:setPosition]
     ]
@@ -47,7 +47,7 @@ module Deck
   # method to return set data
   def set_data(set_num)
     set = SETS.select { |set| set[:setNumber] == set_num }[0]
-    return set.nil? ? nil : [
+    set.nil? ? nil : [
       set[:setNumber],
       set[:setName],
       set[:setLength]
