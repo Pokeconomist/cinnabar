6c857dc894c136077ed898b270450969cbb7fbfd
Pokeconomist <26198040+Pokeconomist@users.noreply.github.com>
2018-01-30 15:12:54 +1100

    Edit docs

--- src/core_extensions/string/titleise.rb
+++ src/core_extensions/string/titleise.rb
@@ -1,6 +1,7 @@
 # Monkey patch on String class.
 class String
   # Capitalises first letter of each word, and downcases all others.
+  #
   # n.b. Capitalisation effective only over ASCII characters.
   # @return [String] Titleised string
   def titleise
