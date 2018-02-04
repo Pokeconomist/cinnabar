7ae4f697571a0848a6e01a1fb0185aff9c613730
Pokeconomist <26198040+Pokeconomist@users.noreply.github.com>
2018-02-04 19:46:54 +1100

    add config files

---
+++ src/config.rb
@@ -0,0 +1,14 @@
+module Cinnabar
+  require 'json'
+  
+  # Config object
+  module Config
+    @config = JSON.parse(File.read('.\data\config.json'), symbolize_names: true)
+    
+    @config.keys.each do |key|
+      self.class.send(:define_method, key) do
+        @config[key]
+      end
+    end
+  end
+end
